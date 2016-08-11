093d07e163ce40b58dc6034a78a9ff2eb9e61213
Vladimir Kamuz <v.kamuz@gmail.com>
2016-08-11 15:46:24 +0300

    Add and install gulp-less, gulp-sourcemaps for gulp and some html files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    sourcemaps = require('gulp-sourcemaps')
     connect = require('gulp-connect');
 
 gulp.task('connect', function() {
@@ -17,19 +19,22 @@ gulp.task('html', function() {
 });
 
 gulp.task('css', function () {
-  return gulp.src('css/*.css')
-    .pipe(connect.reload())
+  return gulp.src('css/*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("css/"))
+    .pipe(connect.reload())
 });
 
 gulp.task('js', function () {
   return gulp.src('js/*.js')
-    .pipe(connect.reload())
     .pipe(gulp.dest("js/"))
+    .pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
-  gulp.watch("css/*.css", ['css']);
+  gulp.watch("css/*.less", ['css']);
   gulp.watch("js/*.js", ['js']);
   gulp.watch("index.html", ['html']);
 });
