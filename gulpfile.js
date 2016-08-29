1946087b06f99470141098ce690a06e79a7c86a9
Vladimir Kamuz <v.kamuz@gmail.com>
2016-08-29 13:21:59 +0300

    Using gulp-rigger and changed structure project

--- gulpfile.js
+++ gulpfile.js
@@ -3,40 +3,36 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     less = require('gulp-less'),
-    sourcemaps = require('gulp-sourcemaps')
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
     connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
-    root: './',
+    root: 'build/',
     livereload: true
   });
 });
 
 gulp.task('html', function() {
-  gulp.src('./*.html')
+  return gulp.src('src/**/*.html')
+  .pipe(rigger())
+  .pipe(gulp.dest("build/"))
   .pipe(connect.reload());
 });
 
 gulp.task('css', function () {
-  return gulp.src('css/*.less')
+  return gulp.src('src/css*.less')
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("css/"))
-    .pipe(connect.reload())
-});
-
-gulp.task('js', function () {
-  return gulp.src('js/*.js')
-    .pipe(gulp.dest("js/"))
+    .pipe(gulp.dest("build/css/"))
     .pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
-  gulp.watch("css/*.less", ['css']);
-  gulp.watch("js/*.js", ['js']);
-  gulp.watch("*.html", ['html']);
+  gulp.watch("src/css/*.less", ['css']);
+  gulp.watch("src/*.html", ['html']);
 });
 
-gulp.task('default', ['watch', 'connect']);
+gulp.task('default', ['watch', 'connect', 'html']);
